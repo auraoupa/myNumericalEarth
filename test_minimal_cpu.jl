bd896204479b4177d28e397e0ee672e5bd8c0afe
Aurélie Albert <auraoupa@gmail.com>
2026-04-07 12:55:32 +0200

    Add minimal CPU test for Oceananigans model

---
+++ test_minimal_cpu.jl
@@ -0,0 +1,18 @@
+using Oceananigans
+Base.retry_load_extensions()
+using Oceananigans
+
+grid = RectilinearGrid(size = (128, 128),
+                              x = (0, 2π),
+                              y = (0, 2π),
+                              topology = (Periodic, Periodic, Flat))
+
+model = NonhydrostaticModel(; grid, advection=WENO())
+
+ϵ(x, y) = 2rand() - 1
+
+set!(model, u=ϵ, v=ϵ)
+
+simulation = Simulation(model; Δt=0.01, stop_iteration=100)
+
+run!(simulation)
